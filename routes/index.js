04fc5e1e75ea22804d774c0d5ae6a5a58cbacfe5
gabriel andrews <gabem_29@yahoo.com>
2022-04-19 23:24:56 -0400

    rout fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api')
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-    res.status(404).send('Page Not Found')
+    res.status(404).send('nothing here')
 });
 
 module.exports = router;
