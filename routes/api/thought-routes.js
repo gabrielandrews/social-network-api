42490dff92b94177dd95fca90a746fcde6fe3031
gabriel andrews <gabem_29@yahoo.com>
2022-04-19 22:57:29 -0400

    routes/ api/ index.js, thought-routes.js, user-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { 
+    addThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought)
+
+
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction)
+
+module.exports = router;
